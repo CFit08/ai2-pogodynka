d61b54b0f454ebdfcd9b3cfe6a4f15106c95ff9f
Colin Fit <fc50430>
2023-11-12 23:39:22 +0100

    labe

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -7,6 +7,7 @@ use App\Repository\MeasurementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class WeatherController extends AbstractController
 {
